6bdf28654b38d1baa4e437e318da55537c7f1f98
Yangtao Lin <yangtaolin938@gmail.com>
2025-08-17 13:48:17 -0400

    added button functionality

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+const editButton = document.querySelector(".profile__edit-button");
+
+const editModal = document.querySelector("#edit-profile-modal");
+const editCloseButton = editModal.querySelector(".modal__close-btn");
+
+const newPostModal = document.querySelector("#new-post-modal");
+const newPostCloseButton = newPostModal.querySelector(".modal__close-btn");
+
+editButton.addEventListener("click", function(){
+    editModal.classList.add("modal_is-opened");
+});
+
+editCloseButton.addEventListener("click", function(){
+    editModal.classList.remove("modal_is-opened");
+});
+
+newPostButton.addEventListener("click", function(){
+    newPostModal.classList.add("modal_is-opened");
+});
+
+newPostCloseButton.addEventListener("click", function(){
+    newPostModal.classList.remove("modal_is-opened");
+});
