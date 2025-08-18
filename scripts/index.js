d09c0df50a623e65ee360f2aaa27e871ecc22baa
Yangtao Lin <yangtaolin938@gmail.com>
2025-08-17 23:07:01 -0400

    Completed Forms and Added Transitions for Modals

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,42 @@
-const editButton = document.querySelector(".profile__edit-button");
+/* For Buttons on Main Page */
+const editProfileButton = document.querySelector(".profile__edit-button");
 const newPostButton = document.querySelector(".profile__new-post");
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__subtitle");
 
-const editModal = document.querySelector("#edit-profile-modal");
-const editCloseButton = editModal.querySelector(".modal__close-btn");
+/*For Edit Profile Modal */
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileCloseButton = editProfileModal.querySelector(".modal__close-btn");
+const editProfileName = editProfileModal.querySelector("#profile_name_input");
+const editProfileDescription = editProfileModal.querySelector("#profile_description_input");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
 
+/* For New Post Modal */
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseButton = newPostModal.querySelector(".modal__close-btn");
+const newPostLink = newPostModal.querySelector("#profile_image_input");
+const newPostCaption = newPostModal.querySelector("#profile_caption_input");
+const newPostForm = newPostModal.querySelector(".modal__form");
 
-editButton.addEventListener("click", function(){
-    editModal.classList.add("modal_is-opened");
+/* Profile Functions */
+editProfileButton.addEventListener("click", function(){
+    editProfileModal.classList.add("modal_is-opened");
+    editProfileName.value = profileName.textContent;
+    editProfileDescription.value = profileDescription.textContent;
 });
 
-editCloseButton.addEventListener("click", function(){
-    editModal.classList.remove("modal_is-opened");
+editProfileCloseButton.addEventListener("click", function(){
+    editProfileModal.classList.remove("modal_is-opened");
 });
 
+editProfileForm.addEventListener("submit", function(evt){
+    evt.preventDefault();
+    profileName.textContent = editProfileName.value;
+    profileDescription.textContent = editProfileDescription.value;
+    editProfileModal.classList.remove("modal_is-opened");
+});
+
+/* New Post Functions */ 
 newPostButton.addEventListener("click", function(){
     newPostModal.classList.add("modal_is-opened");
 });
@@ -22,3 +44,10 @@ newPostButton.addEventListener("click", function(){
 newPostCloseButton.addEventListener("click", function(){
     newPostModal.classList.remove("modal_is-opened");
 });
+
+newPostForm.addEventListener("submit",function(evt){
+    evt.preventDefault();
+    console.log(newPostLink.textcontent);
+    console.log(newPostCaption.textContent);
+    newPostModal.classList.remove("modal_is-opened");
+})
